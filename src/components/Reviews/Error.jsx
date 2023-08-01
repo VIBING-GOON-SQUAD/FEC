de375d34a2cf16cd7df3aa2be93823f3d7c484a2
Dylan W Gordon <dw.gordon2@gmail.com>
2023-08-01 14:43:35 -0700

    Updated gordon to match Origin/Gordon

--- src/components/Reviews/Error.jsx
+++ src/components/Reviews/Error.jsx
@@ -1,23 +1,34 @@
-const Error = ({ handleClose, show}) => {
-    const showHideClassName = show ? "modal display-block" : "modal display-none";
-  
-    return (
-      <div className={showHideClassName}>
-        <div className="TOPBAR"></div>
-        <section className="modal-main">
-        <div className="ErrorHeader">
-        <button className="X" type="button" onClick={handleClose}>
-          </button>
-            <h1 className="Error"> Error</h1>
-       </div>
-        <div className="ErrorBody">
-            <p className="Message">You must be logged in to perform that action.</p>
-          <button className="SignIn" type="button">sign in</button>
-          <button className="Close" type="button" onClick={handleClose}>cancel</button>
-        </div>
-        </section>
-      </div>
-    );
-  };
+const Error = ({ handleClose, show }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
 
-  export default Error;
+  return (
+    <div className={showHideClassName}>
+      <div className="TOPBAR"></div>
+      <section className="modal-main">
+        <div className="ErrorHeader">
+          <button className="X" type="button" onClick={handleClose}></button>
+          <h1 className="Error"> Error</h1>
+        </div>
+        <div className="ErrorBody">
+          <p className="Message">
+            You must be logged in to perform that action.
+          </p>
+          <div className="buttonContainer">
+            <div>
+              <button className="SignIn" type="button">
+                sign in
+              </button>
+            </div>
+            <div>
+              <button className="Close" type="button" onClick={handleClose}>
+                cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Error;
